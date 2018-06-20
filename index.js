9fbea1e2f17d9d91907e1b40359bc11262b60b3d
Scarlett Patton <scarwars05@gmail.com>
2018-06-20 21:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ let randomNumber = Math.random()
 
 let randomInteger = Math.floor(randomNumber)
 
-//let randomUserID = Number.isInteger(randomInteger)
+//let randomInteger = Number.isInteger(randomInteger)
 
