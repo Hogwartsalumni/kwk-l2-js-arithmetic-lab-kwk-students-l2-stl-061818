ac003662f6048b7f0bf3b871f7b26d192d3ad170
Scarlett Patton <scarwars05@gmail.com>
2018-06-20 20:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 let newID = 1000000000 + oldID
 
-function ageIsValid(currentAgeIsInteger) {
-  if (ageIsValid) {
+function ageIsValid() {
+  if (currentAgeIsInteger) {
     true
   } else {
     false
