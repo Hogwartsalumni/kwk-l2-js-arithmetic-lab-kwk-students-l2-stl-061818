aca66569d47871565ca6e8453721cd379f8c840c
Scarlett Patton <scarwars05@gmail.com>
2018-06-20 21:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,4 @@ let randomNumber = Math.random()
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserID >= randomNumber + 1000000001
-let randomUserID <= randomNumber + 1000000020
-
+let randomUserID = randomNumber + 1000000001
